f878798b2b02e6f0fb87bced28505a4ee2bfdd59
Numonovdev <ebxuzsher@gmail.com>
2024-06-11 12:39:08 +0300

    finished

---
+++ JS/index.js
@@ -0,0 +1,367 @@
+// Funksiyaga oid masalalar.
+
+// masala-1
+// function yigindi(a,b){
+//      return a + b;
+// }
+
+// console.log(yigindi(5,9))
+
+// masala 2
+
+// function kop(a,b){
+//      return a * b;
+// }
+// console.log(kop(4,5))
+
+// masala 3
+
+// function kv(a){
+//      return a ** 2;
+// }
+// console.log(kv(8))
+
+// masala 4
+
+// function kv(a){
+//      return a ** 3;
+// }
+// console.log(kv(8))
+
+// masala 5
+
+// function ortachaqiymat(a,b,c){
+//      let orta ;
+//      orta = a + b + c;
+//      return orta / 3;
+// }
+
+// console.log(ortachaqiymat(3,2,2))
+
+// masala 6
+
+// function jufttoq(a){
+     
+//      if(a % 2 == 0){
+//            console.log('true')
+//      } else {
+//            console.log('false')
+//      }
+//      return a;
+// }
+
+// let a = prompt('a sonini kiriting')
+// console.log(jufttoq(a))
+
+// masala 7
+
+
+// function jufttoq(a){
+     
+//      if(a % 2 == 0){
+//            console.log('foalse')
+//      } else {
+//            console.log('true')
+//      }
+//      return a;
+// }
+
+// let a = prompt('a sonini kiriting')
+// console.log(jufttoq(a))
+
+// masala 8
+
+// function max(a,b){
+//      let maxq;
+//      if(a > b){
+//           maxq = a;
+//      } else {
+//           maxq = b;
+//      }
+//      return maxq;
+// }
+// console.log(max(6,7))
+
+// masala 9
+
+
+// function min(a,b){
+//      let minq;
+//      if(a < b){
+//           maxq = a;
+//      } else {
+//           minq = b;
+//      }
+//      return minq;
+// }
+// console.log(min(6,7))
+
+// masala 10
+
+// function yig(a){
+//      let yigindi = 0;
+//      for(let i = 1; i <= a; i++){
+//           yigindi = yigindi + i;
+//      }
+//      return yigindi;
+// }
+
+// console.log(yig(10))
+
+// // masala 11
+
+// function kop(a){
+//      let kopaytma = 1;
+//      for(let i = 1; i <= a; i++){
+//           kopaytma = kopaytma * i;
+//      }
+//      return kopaytma;
+// }
+
+// console.log(kop(3))
+
+// masala 12
+
+// function yosh(a){
+//      if(a >= 18){
+//           console.log('katta')
+//      } else{console.log('kichik')}
+// }
+// yosh(111)
+
+// masala 13
+
+// function truefalse(a,b){
+//      if(a % b == 0){
+//           console.log(true)
+//      } else {console.log('false')}
+// }
+// truefalse(10,3)
+
+// masala 14
+
+// function musbatmanfi(a){
+//      if(a > 0){
+//           console.log('true')
+//      } else if(a < 0) {console.log('false')} else{ console.log('xato bu 0 soni')}
+// }
+// musbatmanfi(7)
+
+// masala 15
+
+// function musbatmanfi(a){
+//      if(a > 0){
+//           console.log('false')
+//      } else if(a < 0) {console.log('true')} else{ console.log('xato bu 0 soni')}
+// }
+// musbatmanfi(7)
+
+// masala 16
+
+// function ildz(a){
+//      let ildizi;
+//      ildizi = Math.sqrt(a);
+//      return ildizi;
+// }
+
+// console.log(ildz(25))
+
+// masala 17
+
+// function farq(a,b){
+//      let farq = a - b;
+//      if(farq < 0){
+//          farq = farq * -1;
+//      } 
+//      return farq;
+// }
+// console.log(farq(5,100))
+
+// masala 18
+
+// function engkotta(a,b,c){
+//      let max;
+//      if(a > b && a > c){
+//           max = a
+//      } else if(b > c && b > a){
+//           max = b
+//      } else if(a < c && c > b){
+//           max = c
+//      }
+//      return max;     
+// }
+
+// console.log(engkotta(5,78,65))
+
+// masala 19
+
+// function engkichik(a,b,c){
+//      let min;
+//      if(a < b && a < c){
+//           min = a
+//      } else if(b < c && b < a){
+//           min = b
+//      } else if(a > c && c < b){
+//           min = c
+//      }
+//      return min;     
+// }
+
+// console.log(engkichik(5,78,65))
+
+// masala 20
+
+// function bigsmol(a){
+//      if(a > 10){
+//           console.log('kotta')
+//      } else if( 10 > a){
+//           console.log('kichik')
+//      } else {console.log('teng')}
+// }
+
+// bigsmol(11)
+
+// masala 21
+// function juft(a,b){
+//      let son = a + b;
+//      if(son % 2 == 0){
+//           console.log('true')
+//      } else console.log('false')
+// }
+// juft(17,97)
+
+// masala 22
+
+// function bew(a){
+//      if(a % 5 == 0){
+//           console.log('true')
+//      } else console.log('false')
+// }
+// bew(31)
+
+// masala 23
+
+// function big(a,b){
+//      if(a > b){
+//           console.log('true')
+//      } else console.log('false')
+// }
+// big(45,32)
+
+
+// masala 24
+
+// function yetti(a){
+//      if(a % 7 == 0){
+//           console.log('true')
+//      } else console.log('false')
+// }
+// yetti(91)
+
+// masala 25
+// function daraja(a){
+//      let darajasi;
+//      darajasi = a ** 10;
+//      return darajasi;
+// }
+// console.log(daraja(2))
+
+
+// masala 26
+
+// function ortacha(a,b){
+//      let orta = a + b;
+//      return orta / 2;
+// }
+// console.log(ortacha(5,8))
+
+// masala 27
+
+// function son(a){
+//      if(a >= 100){
+//           console.log('katta')
+//      } else{console.log('kichik')}
+// }
+// son(71)
+
+
+// masala 28
+
+// function ikki(a){
+//      if(a % 2 == 0){
+//           console.log('true')
+//      } else console.log('false')
+// }
+// ikki(11)
+
+// masala 29
+
+// function kp_ildz(a,b){
+//      let kp = a * b;
+//      kp = Math.sqrt(kp)
+//      return kp;
+// }
+// console.log(kp_ildz(12,3))
+
+// masala 30
+
+// function uchson(a,b,c){
+//      if(a + b == c){
+//           console.log('true')
+//      } else {console.log('false')}
+// }
+// uchson(1,2,3)
+
+
+// For yordamida ishlanadigan masalalar
+
+// masala 1
+
+// function yigindi(a){
+//      let yigindi = 0;
+//     for(let i = 1; i <= 10; i++){
+//      yigindi = yigindi + i;
+//     }
+//     return yigindi;
+// }
+// console.log(yigindi(10))
+
+// masala 2
+// function kopaytma(a){
+//      let kop = 1;
+//      for (let i = 1; i <= 5; i++){
+//           kop = kop * i;
+//      }
+//      return kop;
+// }
+// console.log(kopaytma(5))
+
+// masala 3
+
+// function faktorial(a){
+//      let kop = 1;
+//      for (let i = 1; i <= a; i++){
+//        kop = kop * i
+//      }
+//      return kop;
+// }
+// let a = prompt('a sonini kiriting');
+// console.log(faktorial(a));
+
+// masala 4
+
+// function juft(a){
+//      for(let i = 2; i <= a; i += 2){
+//           console.log(i);
+//      }
+// }
+// juft(84)
+
+// masala 5
+
+function toq(a){
+     for(let i = 1; i <= a; i += 2){
+          console.log(i);
+     }
+}
+toq(73)
